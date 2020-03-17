b811b9ee958e4e3b940c48694a1c0dddc2818895
Adela Darmansyah <52761827+ayohana@users.noreply.github.com>
2020-03-17 10:30:10 -0700

    Initial commit

---
+++ ProjectName.Solution/ProjectName/Controllers/ClassesNameController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ProjectName.Models;
+
+namespace ProjectName.Controllers
+{
+  public class ClassesNameController : Controller
+  {
+
+    // [HttpGet("/classesname")]
+    // public ActionResult Index()
+    // {
+    //   return View();
+    // }
+
+  }
+}
